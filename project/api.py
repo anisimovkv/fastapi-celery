1c0af059e69d3d9d9cadb8ddb6116de64095cfb4
konstantin.anisimov <konstantin.anisimov>
2021-12-19 11:47:07 +0200

    added: celery for project

--- project/api.py
+++ project/api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi import Body
 from fastapi import Request
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from celery.result import AsyncResult
+
+from worker import create_task
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
@@ -12,3 +17,21 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def home(request: Request):
     return templates.TemplateResponse(
         'home.html', context={'request': request})
+
+
+@app.post("/tasks", status_code=201)
+def run_task(payload=Body(...)):
+    task_type = payload['type']
+    task = create_task.delay(int(task_type))
+    return JSONResponse({'task_id': task.id})
+
+
+@app.get('/tasks/{task_id}')
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
