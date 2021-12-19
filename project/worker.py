091a8f4ba25526f42b5c9117b8341576cc4e0cbd
konstantin.anisimov <konstantin.anisimov>
2021-12-19 23:42:04 +0200

    checked: celery worker with redis

--- project/worker.py
+++ project/worker.py
@@ -5,9 +5,9 @@ from celery import Celery
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get(
-    "CELERY_BROKER_URL", "redis://localhost:6379")
+    "CELERY_BROKER_URL", "redis://redis:6379")
 celery.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379")
+    "CELERY_RESULT_BACKEND", "redis://redis:6379")
 
 
 @celery.task(name='create_task')
